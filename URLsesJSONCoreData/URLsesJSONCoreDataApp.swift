d053f0a423e493be42c336d2ed43191f211cd29a
DevDataOrg <48391348+DevDataOrg@users.noreply.github.com>
2021-11-25 18:39:34 +0300

    Initial Commit

---
+++ URLsesJSONCoreData/URLsesJSONCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLsesJSONCoreDataApp.swift
+//  URLsesJSONCoreData
+//
+//  Created by Artyom Romanchuk on 25.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct URLsesJSONCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
