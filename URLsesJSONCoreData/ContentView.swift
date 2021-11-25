2c6f90378f680c105129c0b79b164de456f67bf4
DevDataOrg <48391348+DevDataOrg@users.noreply.github.com>
2021-11-25 18:57:53 +0300

    all

--- URLsesJSONCoreData/ContentView.swift
+++ URLsesJSONCoreData/ContentView.swift
@@ -7,12 +7,53 @@
 
 import SwiftUI
 
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
+
 struct ContentView: View {
+    @State private var results = [Result]()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                Text(item.collectionName)
+            }
+        }
+        .task {
+            await loadData()
+        }
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+
+            // more code to come
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedResponse.results
+            }
+            
+        } catch {
+            print("Invalid data")
+        }
+        
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
